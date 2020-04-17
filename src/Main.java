cbc46bbb8802ee471870fc75863244cdd067398f
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-04-17 23:18:35 +0300

    max int in array

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main
 {
 
+
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        int[] arr = {5,6,9,8,4,21,54,65,99,32};
+        if(arr.length > 0)
+        {
+            int max = arr[0];
+            for (int i = 0; i < arr.length; i++)
+            {
+                if (max > arr[i]) {
+                    max = arr[i];
+                }
+            }
+            System.out.println(max);
+        }
     }
 }
