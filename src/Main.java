6fbbdc8552131276c905c70852d553b2de87e884
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-04-19 21:26:34 +0300

    learn cycles while and do while

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 public class Main
 {
-
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        String string = "";
+
+        while(string.length() < 36)
+        {
+            string+="Hello World \n";
+        }
+
+        System.out.println(string);
+        doWhile();
+    }
+
+    public static void doWhile()
+    {
+        int c = 10;
+        do
+        {
+            System.out.println("осталось " + c + " секунд \n");
+            c--;
+        } while (c > 0);
     }
 }
