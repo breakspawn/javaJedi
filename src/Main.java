fae5b88348d0469878d5cbe7dabf501dcb26e886
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-04-20 18:17:22 +0300

    классы и функции

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        ReaderConsole reader = new ReaderConsole();
+        try
+        {
+            int x = reader.getInt();
+            System.out.println(x);
+        } catch (NumberFormatException nfe) {
+            System.out.println("неправильное значение");
+        }
+    }
+}
+
+class ReaderConsole
+{
+    Scanner scanner;
+
+    public ReaderConsole()
+    {
+        scanner = new Scanner(System.in);
+    }
+    
+    public int getInt() throws NumberFormatException
+    {
+        String s = scanner.next();
+        int number = Integer.valueOf(s);
+        return number;
     }
 }
