599869be448fc35d800925f838d1c78af3a0637d
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-04-21 17:46:50 +0300

    декомпозиция после

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ public class Main
 
     public static void main(String[] args)
     {
-
+        try {
+            Parser parser = new Parser(new File("C:\\","ParserShitCode.txt"));
+            parser.fromConsole();
+            parser.toConsole();
+        } catch (IOException ioException) {}
     }
 }
 
@@ -16,41 +20,46 @@ class Parser {
     public Parser(File f){
         file = f;
     }
-
-    public String read(boolean outToConsole) throws IOException {
+    // метод возвравата данных в строку
+    public String read() throws IOException {
         InputStream in = new FileInputStream(file);
         String output = "";
-        int bt = 0;
-        do{
+        int bt = in.read();
+        while (bt > 0) {
+            output += (char) bt;
             bt = in.read();
-            if(bt > 0)
-                output += (char) bt;
-        }while(bt > 0);
-
-        if(outToConsole)
-        {
-            System.out.print(output);
-            return "";
         }
-        else
-        {
-            return output;
-        }
+        return output;
     }
 
-    public void write(String content, boolean fromConsole) throws IOException
-    {
-        if(fromConsole)
-        {
-            Scanner scan = new Scanner(System.in);
-            content = scan.nextLine();
-        }
+    public void toConsole() throws IOException {
+        System.out.println(read());
+    }
 
+
+    // была функция которая записывала в файл содержимое из консоли или то что мы передали
+    // 1. Запись в файл из консоли
+    // 2. Запись в файл переданного содержимого
+    // разбить:
+
+    // 1.
+    public void fromConsole() throws IOException {
+        Scanner scan = new Scanner(System.in);
+        String content = scan.nextLine();
+        write(content);
+    }
+
+    // 2.
+    public void write(String content) throws IOException {
         OutputStream os = new FileOutputStream(file);
-
         for(int i = 0; i < content.length(); i++)
         {
             os.write(content.charAt(i));
         }
     }
+
+
+
+
+
 }
