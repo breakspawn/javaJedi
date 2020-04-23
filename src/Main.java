0c686d04ddc791894f124fa6f212b873d7b9f0c8
breakspawn <59422004+breakspawn@users.noreply.github.com>
2020-04-23 17:51:28 +0300

    методы копирования чтения замена символов и запись в новый файл

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,84 @@
+import java.io.*;
+
 public class Main
 {
 
-    public static void main(String[] args)
+
+    File f;
+    String result;
+
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Hello World");
+        //Копирование файлов
+      CopyFiles copyFiles = new CopyFiles();
+      File from = new File("c:\\", "Shitcode");
+      File to =  new File("c:\\", "Shitcode0000");
+      copyFiles.CopyFiles(from, to);
+
+//замена текста файла на заглавные буквы
+        FileToText rf = new FileToText();
+        String c = rf.readFile("c:\\Shitcode");
+        String res = c.toUpperCase();
+        File f = new File("c:\\", "abc");
+        System.out.println(res);
+        OutputStream os = new FileOutputStream(f);
+        for (int i = 0; i < res.length(); i++) {
+            os.write(res.charAt(i));
+
+        }
+
     }
 }
+//считываю символы из файла и возвращаю строку
+class FileToText {
+    public String readFile(String fN) throws FileNotFoundException {
+        String str = "";
+
+        try (FileReader fr = new FileReader(fN)) {
+            int c;
+            while ((c = fr.read()) != -1) {
+                str += (char)c;
+
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return str;
+    }
+    public void writeTextToFile (String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+
+    }
+ }
+
+
+class CopyFiles
+{
+    //метод потокового копирования файлов
+    public void CopyFiles (File source,File dest) throws IOException
+    {
+        InputStream is = null;
+        OutputStream os = null;
+        try
+        {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0)
+            {
+                os.write(buffer, 0, length);
+            }
+        } finally
+        {
+            is.close();
+            os.close();
+        }
+    }
+}
+
+
+
+
+
